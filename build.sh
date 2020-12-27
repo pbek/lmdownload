1262c9055eddd79775a6ee7e2240ae4def22a370
Patrizio Bekerle <patrizio@bekerle.com>
2020-12-27 20:31:56 +0100

    Migrate to docker module

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 export VERSION_NUMBER=`cat version`
 echo Getting dependencies...
-go get
+go mod download
 echo Building version $VERSION_NUMBER...
 go build -ldflags "-X main.version=$VERSION_NUMBER" -o bin/lmdownload -i .
 echo Done
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/pbek/lmdownload
+
+go 1.15
+
+require (
+	github.com/go-ini/ini v1.62.0
+	github.com/henrylee2cn/surfer v1.2.3
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+)
